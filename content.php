13315f187d5d053c44957edd2dc21d5c20b62c3a
Unfiltered.Bullfrog117.com <wp_nu6wc8@wp181581.dreamhostps.com>
2014-02-18 17:01:59 -0800

    circa 2014-02-18

--- content.php
+++ content.php
@@ -21,10 +21,11 @@
 				http://codex.wordpress.org/Post_Thumbnails
 			-->
 
-			<?php the_post_thumbnail('medium', array('class' => 'aligncenter')); ?>
 			<?php if ( is_single() ) : ?>
+			<?php the_post_thumbnail('large', array('class' => 'aligncenter')); ?>
 			<h1 class="entry-title"><?php the_title(); ?></h1>
 			<?php else : ?>
+			<?php the_post_thumbnail('medium', array('class' => 'aligncenter')); ?>
 			<h1 class="entry-title">
 				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'twentytwelve' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 			</h1>
